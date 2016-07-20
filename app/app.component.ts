936e27ddbbe073cff83d0dae300e243cb77faad9
lisawa <skyblade927@hotmail.com>
2016-07-20 11:08:55 +0800

    update page

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'template/MainPage.html',
+})
+
+export class AppComponent{
+    constructor(){
+        
+    }
+}
