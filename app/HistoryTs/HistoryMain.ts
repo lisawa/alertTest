fa8baaff742de53c9c93b41fad0e6c918b90be82
wyc730 <james.wyc@gmail.com>
2016-07-26 09:49:31 +0800

    Add History Tab
    Add History Tab

--- app/HistoryTs/HistoryMain.ts
+++ app/HistoryTs/HistoryMain.ts
@@ -1,7 +1,10 @@
 import {Component, Input, Output, EventEmitter} from 'angular2/core';
+import {InkButton} from '../TestComponent/InkButton';
+
 
 @Component({
     selector: 'HistoryMain',
+    directives: [InkButton,],
     templateUrl: 'template/History/HistoryMain.html',
 })
 
